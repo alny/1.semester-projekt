421b6937677f1004ddeb44924dbba2aba5eb0ef2
drixmix <foxyhorse@gmail.com>
2017-12-05 10:02:08 +0100

    product controller boilerplate

--- VestbjergByggecenter/ControllerLayer/ProductController.java
+++ VestbjergByggecenter/ControllerLayer/ProductController.java
@@ -10,26 +10,15 @@ package ControllerLayer;
 public class ProductController
 {
     // instansvariabler - erstat eksemplet herunder med dine egne variabler
-    private int x;
-
+   
     /**
      * Konstruktør for objekter af klassen ProductController
      */
     public ProductController()
     {
         // initialiser instansvariable
-        x = 0;
+        ;
     }
 
-    /**
-     * Et eksempel på en metode - erstat denne kommentar med din egen
-     * 
-     * @param  y  eksempel på en parameter til en metode
-     * @return    summen af x og y 
-     */
-    public int sampleMethod(int y)
-    {
-        // indsæt din egen kode her
-        return x + y;
-    }
+ 
 }
