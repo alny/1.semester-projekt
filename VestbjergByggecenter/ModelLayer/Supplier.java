3e4f85391599bd0ceb6413cd4a294f6823d5c394
drixmix <foxyhorse@gmail.com>
2017-12-05 12:40:17 +0100

    Merged

---
+++ VestbjergByggecenter/ModelLayer/Supplier.java
@@ -0,0 +1,24 @@
+package ModelLayer;
+import java.util.LinkedList;
+
+/**
+ * Write a description of class Supplier here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Supplier
+{
+    private String firmName;
+    private int amountProduct;
+    private LinkedList<Product> listOfProducts;
+
+    public Supplier()
+    {
+        listOfProducts = new LinkedList<Product>();
+    }
+
+    public void addProduct(){
+        
+    }
+}
