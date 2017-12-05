aebb6b31e4f24cc70402a8ca80f1da81df4ec392
silentbessh <silentbessh@gmail.com>
2017-12-05 09:43:43 +0100

    AccountController update

--- VestbjergByggecenter/ControllerLayer/AccountController.java
+++ VestbjergByggecenter/ControllerLayer/AccountController.java
@@ -1,7 +1,7 @@
 package ControllerLayer;
 import ModelLayer.AccountContainer;
-
-
+import ModelLayer.Account;
+import ModelLayer.Customer;
 
 /**
  * Lav en beskrivelse af klassen CustomerController her.
@@ -19,9 +19,22 @@ public class AccountController
     accountContainer = AccountContainer.getInstance();
     }
     
-    public void updatePhone(String phone, String newPhone){
-              
-
-   
-}
+    public String updatePhone(String phone, String newPhone){
+        return accountContainer.updatePhone(phone, newPhone);
+    }
+    
+    public String updateAddress(String phone, String newAddress){
+        return accountContainer.updateaddress(phone, newAddress);
+    }
+    
+    public String createAccount(String name, String address, 
+    String zip, String city, String phone, String type){
+        Account customer = new Customer(name,address,zip,city,phone,type);
+        accountContainer.addAccount(customer);
+        return "Succes: account oprettet";
+    }
+    
+    public Account findCustomer(String phone){
+        return accountContainer.findAccount(phone);
+    }
 }
